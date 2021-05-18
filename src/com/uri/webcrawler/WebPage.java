9951bd9cc5fb5e77f35e328653662bb0f6b651d1
urimem <uri.mailbox@gmail.com>
2021-05-18 16:40:08 +0300

    Graph print method added

--- src/com/uri/webcrawler/WebPage.java
+++ src/com/uri/webcrawler/WebPage.java
@@ -1,5 +1,6 @@
 package com.uri.webcrawler;
 
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
 class WebPage {
@@ -9,6 +10,15 @@ class WebPage {
 
     private WebPage(String url) {
         this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Iterator<WebPage> getLinkedPages()
+    {
+        return (Iterator<WebPage>) linkedPages.values();
     }
 
     public void addLinkedPage(WebPage page) {
--- src/com/uri/webcrawler/WebPageGraph.java
+++ src/com/uri/webcrawler/WebPageGraph.java
@@ -1,10 +1,13 @@
 package com.uri.webcrawler;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WebPageGraph {
     // Key = Full URL string
-    // TODO: remove the schema/protocol part
+    // TODO: remove the schema/protocol part, remove #fragment part
     private ConcurrentHashMap<String, WebPage> pages = new ConcurrentHashMap<>();
     private final String rootPageUrl;
 
@@ -12,6 +15,30 @@ public class WebPageGraph {
         this.rootPageUrl = rootPageUrl;
     }
 
-    //public void
+    //contains
 
+    //add
+
+    @Override
+    public String toString() {
+        Set<String> printedPageUrls = new HashSet<String>();
+        StringBuffer result = new StringBuffer();
+        graphTraverse(printedPageUrls, result,pages.get(rootPageUrl), "");
+        return result.toString();
+    }
+
+    // Recursive print method - DFS traverse, not drilling to child pageas if page already printed.
+    private void graphTraverse(Set<String> printedPageUrls, StringBuffer result, WebPage page, String indent) {
+        if (printedPageUrls.contains(page.getUrl())) {
+            result.append(indent).append(page.getUrl()).append('\n');
+        }
+        else {
+            result.append(indent).append(page.getUrl()).append('\n');
+            printedPageUrls.add(page.getUrl());
+            for (Iterator<WebPage> it = page.getLinkedPages(); it.hasNext(); ) {
+                WebPage child = it.next();
+                graphTraverse(printedPageUrls, result,child, indent+'\t');
+            }
+        }
+    }
 }
